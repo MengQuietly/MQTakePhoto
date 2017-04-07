73319571659057abdf124f07adc9ef714c3ac06c
mengmeng <wenjing@txdev.org>
2017-04-07 21:13:06 +0800

    Initial Commit

---
+++ MQPhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQPhotoDemo
+//
+//  Created by mengmeng on 2017/4/7.
+//  Copyright © 2017年 mengmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
